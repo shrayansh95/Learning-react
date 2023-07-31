4e4f7703bc2e0d8c7e49c949690cb416fc8760e1
shrayansh95 <mailshrayansh95@gmail.com>
2023-07-31 18:35:49 +0530

    Learnt about JSX

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+// JSX => Babel transpiles it to React.createElement => JS Object => HTMLELEMENT(render)
+// This is a React Element
+const heading = <h1 id="heading">Hello World from jsx</h1>;
 
-const h1 = React.createElement("h1", {}, "I am a h1 tag");
+const Title = () => <h1 id="heading">Hello World from jsx</h1>;
 
-const h2 = React.createElement("h2", {}, "This is an h2 tag");
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [h1, h2]),
-  React.createElement("div", { id: "child2" }, [h1, h2]),
-]);
-
-console.log(parent); // This is an object not an html element
+// This is a React Functional Component
+const HeadingComponent = () => (
+  <div className="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Hello From Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent); // This method is responsible to take the React Element object and convert it to a html element and place it inside the root element
+root.render(<HeadingComponent />);
